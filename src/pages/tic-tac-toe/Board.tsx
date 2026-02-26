d307475ffb7c15452893c126cb8da2351ebc2751
forks <redforks@gmail.com>
2026-02-26 17:04:49 +0800

    Refactor Board layout to use simple divs instead of table display

--- src/pages/tic-tac-toe/Board.tsx
+++ src/pages/tic-tac-toe/Board.tsx
@@ -9,7 +9,7 @@ function Square({ value, onClick }: SquareProps) {
   return (
     <button
       type="button"
-      className="bg-white border border-zinc-500 text-2xl font-bold size-8 -mr-px -mt-px text-center"
+      className="bg-white border border-zinc-500 text-2xl font-bold size-8 -mr-px -mt-px align-top"
       onClick={onClick}
     >
       {value}
@@ -39,18 +39,18 @@ export function Board({ squares, currentPlayer, onPlay }: BoardProps) {
       <div className="mb-4 text-xl font-semibold">
         {winner ? `Winner: ${winner}` : `Current player: ${currentPlayer}`}
       </div>
-      <div className="table border-collapse">
-        <div className="table-row">
+      <div>
+        <div>
           <Square value={squares[0]} onClick={() => handleClick(0)} />
           <Square value={squares[1]} onClick={() => handleClick(1)} />
           <Square value={squares[2]} onClick={() => handleClick(2)} />
         </div>
-        <div className="table-row">
+        <div>
           <Square value={squares[3]} onClick={() => handleClick(3)} />
           <Square value={squares[4]} onClick={() => handleClick(4)} />
           <Square value={squares[5]} onClick={() => handleClick(5)} />
         </div>
-        <div className="table-row">
+        <div>
           <Square value={squares[6]} onClick={() => handleClick(6)} />
           <Square value={squares[7]} onClick={() => handleClick(7)} />
           <Square value={squares[8]} onClick={() => handleClick(8)} />
