b2f721fd07dde1ada3d52150317c8a20eef28bc5
forks <redforks@gmail.com>
2026-03-08 20:05:43 +0800

    Init chadcn

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { mockDevServerPlugin } from 'vite-plugin-mock-dev-server'
 import { defineConfig } from 'vitest/config'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,6 +20,11 @@ export default defineConfig({
     proxy: {
       '^/api': 'http://blha.com/',
     },
+  },
+  resolve: {
+      alias: {
+          "@": path.resolve(__dirname, "src"),
+      }
   },
   test: {
     environment: 'happy-dom',
